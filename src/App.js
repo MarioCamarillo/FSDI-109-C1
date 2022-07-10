914b7a954c6522fbc3ed17d275b8489f5721d19e
MarioCamarillo <104475782+MarioCamarillo@users.noreply.github.com>
2022-07-10 10:57:13 -0700

    FSDI 109 #1

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/navbar';
+import Footer from './components/Footer';
+import Catalog from './components/catalog';
+import About from './components/about';
+import Home from './components/home';
+import Cart from './components/cart';
+
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <BrowserRouter>
+      <Navbar></Navbar>
+
+
+      <Routes>
+        <Route path='/'         element={ <Home />} />
+        <Route path='/catalog'  element={ <Catalog />} />
+        <Route path='/about'    element={ <About /> } />
+        <Route path='/cart'     element={ <Cart /> } />
+      </Routes>
+
+      <Footer></Footer>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
